42708d6ee6884faeba47cff9b18cb8a7f5146899
oscar <oscar@atomx.top>
2026-03-12 16:19:09 +0800

    fix: import prisma default

--- app/(main)/storyboard-gen/[taskId]/page.tsx
+++ app/(main)/storyboard-gen/[taskId]/page.tsx
@@ -1,5 +1,5 @@
 
-import { prisma } from '@/lib/prisma';
+import prisma from '@/lib/prisma';
 import { notFound } from 'next/navigation';
 import { StoryboardGenDetail } from './StoryboardGenDetail';
 
